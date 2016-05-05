0a396dfb30ec7fdc544d7ffc1adf4d432cbe417d
Akarachai Passavoranan <akarachai45525@hotmail.com>
2016-05-05 17:04:20 +0700

    2.8 all finished

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -30,15 +30,21 @@ public class StudentApp {
 		}
 //		students.stream().filter(filter).sorted(sorter).forEach(action);
 	}
-
 	public static void main(String[] args) {
 		LocalDate localDate = LocalDate.now();
 		Predicate<Student> birthdayTest = (s) -> s.getBirthdate().getMonthValue() == localDate.getMonthValue();
+		Predicate<Student> inTwoWeeks = (s) -> s.getBirthdate().getDayOfYear() >= localDate.getDayOfYear() && s.getBirthdate().getDayOfYear() <= localDate.getDayOfYear()+14;
 		Consumer<Student> toString = (s) -> System.out.printf("%s %s will have birthday on %d %s.\n", s.getFirstname(),
-				s.getLastname(), s.getBirthdate().getDayOfMonth(), localDate.getMonth());
+				s.getLastname(), s.getBirthdate().getDayOfMonth(), s.getBirthdate().getMonth());
 		List<Student> students = Registrar.getInstance().getStudents();
-		Comparator<Student> sorter = (s1,s2) -> s1.getFirstname().charAt(0) - s2.getFirstname().charAt(0);
+		Comparator<Student> byName = (s1,s2) -> s1.getFirstname().charAt(0) - s2.getFirstname().charAt(0);
+		Comparator<Student> byBirthday = (s1,s2) -> s1.getBirthdate().getDayOfMonth() - s2.getBirthdate().getDayOfMonth();
 		StudentApp app = new StudentApp();
-		app.filterAndPrint(students, birthdayTest, toString, sorter);
+		System.out.println("Sort by name: ");
+		app.filterAndPrint(students, birthdayTest, toString, byName);
+		System.out.println("\nSort by birthday: ");
+		app.filterAndPrint(students, birthdayTest, toString, byBirthday);
+		System.out.println("\nList of people who will have birthday in two weeks: ");
+		app.filterAndPrint(students, inTwoWeeks, toString, byBirthday);
 	}
 }
