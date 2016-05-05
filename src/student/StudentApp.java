bd3663afbabe01f24f221a034e73aada8212a48d
Akarachai Passavoranan <akarachai45525@hotmail.com>
2016-05-05 16:01:05 +0700

    problem 2 first commit

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -1,8 +1,13 @@
 package student;
+
+import java.time.LocalDate;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * Display reminders of students having a birthday soon.
+ * 
  * @author you
  */
 public class StudentApp {
@@ -10,19 +15,25 @@ public class StudentApp {
 	/**
 	 * Print the names (and birthdays) of students having a birtday in the
 	 * specified month.
-	 * @param students list of students
-	 * @param month the month to use in selecting bithdays
+	 * 
+	 * @param students
+	 *            list of students
+	 * @param month
+	 *            the month to use in selecting bithdays
 	 */
-	public void filterAndPrint( List<Student> students, int month ) {
-		for(Student s : students ) {
-			if (s.getBirthdate().getMonthValue() == month)
-	                  System.out.println( s );
+	public void filterAndPrint(List<Student> students, Predicate<Student> filter, Consumer<Student> action) {
+		for (Student s : students) {
+			if (filter.test(s))
+				action.accept(s);
 		}
 	}
-	
+
 	public static void main(String[] args) {
+		LocalDate localDate = LocalDate.now();
+		Predicate<Student> birthdayTest = (s) -> s.getBirthdate().getMonthValue() == localDate.getMonthValue();
+		Consumer<Student> toString = (s) -> System.out.printf("%s %s will have birthday on %d %s.\n", s.getFirstname(), s.getLastname(), s.getBirthdate().getDayOfMonth(), localDate.getMonth());
 		List<Student> students = Registrar.getInstance().getStudents();
 		StudentApp app = new StudentApp();
-		app.filterAndPrint(students, 5 /* may */);
+		app.filterAndPrint(students, birthdayTest, toString);
 	}
 }
