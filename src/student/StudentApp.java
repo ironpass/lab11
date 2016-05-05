3ad2ec5ce3c67b44e182d15416355aa5be748628
Akarachai Passavoranan <akarachai45525@hotmail.com>
2016-05-05 16:26:13 +0700

    problem 2 finished (without optional problems)

--- src/student/StudentApp.java
+++ src/student/StudentApp.java
@@ -1,6 +1,7 @@
 package student;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
@@ -21,19 +22,23 @@ public class StudentApp {
 	 * @param month
 	 *            the month to use in selecting bithdays
 	 */
-	public void filterAndPrint(List<Student> students, Predicate<Student> filter, Consumer<Student> action) {
+	public void filterAndPrint(List<Student> students, Predicate<Student> filter, Consumer<Student> action, Comparator<Student> sorter) {
+		students.sort(sorter);
 		for (Student s : students) {
-			if (filter.test(s))
+			if (filter.test(s)) 
 				action.accept(s);
 		}
+		
 	}
 
 	public static void main(String[] args) {
 		LocalDate localDate = LocalDate.now();
 		Predicate<Student> birthdayTest = (s) -> s.getBirthdate().getMonthValue() == localDate.getMonthValue();
-		Consumer<Student> toString = (s) -> System.out.printf("%s %s will have birthday on %d %s.\n", s.getFirstname(), s.getLastname(), s.getBirthdate().getDayOfMonth(), localDate.getMonth());
+		Consumer<Student> toString = (s) -> System.out.printf("%s %s will have birthday on %d %s.\n", s.getFirstname(),
+				s.getLastname(), s.getBirthdate().getDayOfMonth(), localDate.getMonth());
 		List<Student> students = Registrar.getInstance().getStudents();
+		Comparator<Student> sorter = (s1,s2) -> s1.getFirstname().charAt(0) - s2.getFirstname().charAt(0);
 		StudentApp app = new StudentApp();
-		app.filterAndPrint(students, birthdayTest, toString);
+		app.filterAndPrint(students, birthdayTest, toString, sorter);
 	}
 }
